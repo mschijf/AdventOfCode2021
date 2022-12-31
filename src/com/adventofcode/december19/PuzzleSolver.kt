aaceb4de12a50f03a2ba86f8219beed90aa9035e
mschijf <mschijf@gmail.com>
2022-12-31 12:10:38 +0100

    December 19 (not done) and December 20 (done)

---
+++ src/com/adventofcode/december19/PuzzleSolver.kt
@@ -0,0 +1,16 @@
+package com.adventofcode.december19
+
+import com.adventofcode.PuzzleSolverAbstract
+import java.lang.Long.max
+
+fun main() {
+    PuzzleSolver(test=false).showResult()
+}
+
+class PuzzleSolver(test: Boolean) : PuzzleSolverAbstract(test) {
+
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
